9a2231976a384d3bb208d3c7781941a99f0d92f1
Rapetti Matias <rapettimatias97@gmail.com>
2023-10-20 09:04:14 -0300

    nuevo commi

--- client/src/components/CardsContainer/CardsContainer.jsx
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -1,53 +1,52 @@
 /* eslint-disable react/prop-types */
-import styles from './CardsContainer.module.css'
-import { useState } from 'react'
-import Card from '../Card/Card'
-import Pagination from '../Pagination/Pagination'
-import Filters from '../Filters/Filters'
+import styles from "./CardsContainer.module.css";
+import { useState } from "react";
+import Card from "../Card/Card";
+import Pagination from "../Pagination/Pagination";
+import Filters from "../Filters/Filters";
 
+const CardsContainer = ({ allDogs }) => {
+  //Total de perros
+  const totalDogs = allDogs.length;
 
-const CardsContainer = ({allDogs})=>{
+  //Paginación
+  const [currentPage, setCurrentPage] = useState(1);
 
-    //Total de perros
-    const totalDogs = allDogs.length;
+  //Cantidad de perros por página
+  const dogsByPage = 8;
+  const lastIndex = currentPage * dogsByPage; // 8
+  const firstIndex = lastIndex - dogsByPage; // 0
 
-    //Paginación
-    const [currentPage, setCurrentPage] = useState(1);
-
-    //Cantidad de perros por página
-    const dogsByPage = 8;
-    const lastIndex = currentPage * dogsByPage; // 8
-    const firstIndex = lastIndex - dogsByPage; // 0
-
-    return(
-        <>
-            <Filters/>
-            <div className="container">
-                <div className={styles.cardDog}>
-                    {allDogs.map( dog =>{
-                        return (
-                            <Card
-                                key={dog.id} 
-                                id = {dog.id}
-                                image = {dog.image}
-                                name = {dog.name}
-                                temperaments={dog.temperaments}
-                                minWeight={dog.minWeight}
-                                maxWeight={dog.maxWeight}
-                            />
-                        )
-                    }).slice(firstIndex,lastIndex)}
-                </div>
-            </div>
-            <Pagination 
-            totalDogs={totalDogs}
-            dogsByPage ={dogsByPage}
-            currentPage ={currentPage}
-            setCurrentPage={setCurrentPage}
-            />
-        </>
-        
-    )
-}
+  return (
+    <>
+      <Filters />
+      <div className="container">
+        <div className={styles.cardDog}>
+          {allDogs
+            .map((dog) => {
+              return (
+                <Card
+                  key={dog.id}
+                  id={dog.id}
+                  image={dog.image}
+                  name={dog.name}
+                  temperaments={dog.temperaments}
+                  minWeight={dog.minWeight}
+                  maxWeight={dog.maxWeight}
+                />
+              );
+            })
+            .slice(firstIndex, lastIndex)}
+        </div>
+      </div>
+      <Pagination
+        totalDogs={totalDogs}
+        dogsByPage={dogsByPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </>
+  );
+};
 
 export default CardsContainer;
